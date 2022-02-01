b7f295bf1b284593c1ee965e8bae1e9b567995fd
Adrien Delorme <azr@users.noreply.github.com>
2022-02-01 16:16:41 +0100

    Create go.sh

---
+++ zsh.d/go.sh
@@ -0,0 +1,12 @@
+
+# Configure our private repositories. By blacklisting personal and work repos.
+export GOPRIVATE="github.com/azr,github.com/hashicorp"
+
+# allow doing cd gitlab.com/...
+export CDPATH=$CDPATH:$(go env GOPATH)/src
+
+# allow doing cd azr/...
+export CDPATH=$CDPATH:$(go env GOPATH)/src/github.com
+
+# add go as a binary
+export PATH=$PATH:$(go env GOPATH)/bin
