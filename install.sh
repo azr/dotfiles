306d9d45f6ac4fac94b3f47a1b30509449298eaa
Adrien Delorme <azr@users.noreply.github.com>
2022-02-01 17:28:46 +0100

    self hint for future

--- install.sh
+++ install.sh
@@ -47,3 +47,5 @@ done
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo now add /opt/homebrew/bin/zsh to /etc/shells and run chsh -s /opt/homebrew/bin/zsh
