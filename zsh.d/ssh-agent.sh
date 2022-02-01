2a267b171492f0b053511e68cdac8b6d3fe7512e
Adrien Delorme <azr@users.noreply.github.com>
2022-02-01 16:29:29 +0100

    Update ssh-agent.sh
    add more stuff

--- zsh.d/ssh-agent.sh
+++ zsh.d/ssh-agent.sh
@@ -1,2 +1,39 @@
+#-------------------------------------------------------------------------------
+# SSH Agent
+#-------------------------------------------------------------------------------
+SSH_ENV=$HOME/.ssh/environment
 
-eval "$(ssh-agent)"
+function start_ssh_agent {
+    if [ ! -x "$(command -v ssh-agent)" ]; then
+        return
+    fi
+
+    if [ ! -d "$(dirname $SSH_ENV)" ]; then
+        mkdir -p $(dirname $SSH_ENV)
+        chmod 0700 $(dirname $SSH_ENV)
+    fi
+
+    ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
+    chmod 0600 ${SSH_ENV}
+    . ${SSH_ENV} > /dev/null
+    ssh-add
+}
+
+# Source SSH agent settings if it is already running, otherwise start
+# up the agent proprely.
+if [ -f "${SSH_ENV}" ]; then
+     . ${SSH_ENV} > /dev/null
+     # ps ${SSH_AGENT_PID} doesn't work under cywgin
+     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+         start_ssh_agent
+     }
+else
+    case $UNAME in
+      MINGW*)
+        ;;
+      *)
+        start_ssh_agent
+        ;;
+    esac
+fi
+
